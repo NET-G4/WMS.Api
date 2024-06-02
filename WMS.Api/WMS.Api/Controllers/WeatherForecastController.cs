5e4967d2ca3a316c180c207898ae108cf4c87ed6
aXayitov <137428961+aXayitov@users.noreply.github.com>
2024-06-02 17:43:32 +0500

    code review fixes.

--- WMS.Api/WMS.Api/Controllers/WeatherForecastController.cs
+++ WMS.Api/WMS.Api/Controllers/WeatherForecastController.cs
@@ -6,10 +6,17 @@ namespace WMS.Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly string[] Summaries = [
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"];
 
         private readonly ILogger<WeatherForecastController> _logger;
 
