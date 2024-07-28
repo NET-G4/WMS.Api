43bbf07fb56373cb4d4f0f582cc6eef5cf0f2fce
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2024-07-28 11:42:55 -0700

    add swagger authentication

--- WMS.Api/WMS.Api/Extensions/DependencyInjection.cs
+++ WMS.Api/WMS.Api/Extensions/DependencyInjection.cs
@@ -1,52 +1,74 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Syncfusion.Licensing;
 using System.Reflection;
 using System.Text;
 using WMS.Domain.Entities.Identity;
 using WMS.Infrastructure.Configurations;
 using WMS.Infrastructure.Persistence;
-using WMS.Infrastructure.Persistence.Migrations;
 using WMS.Services;
 using WMS.Services.Interfaces;
 using WMS.Services.Mappings;
 
 namespace WMS.Api.Extensions;
 
-public static class DependencyInjection
+internal static class DependencyInjection
 {
     public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        AddConfigurationOptions(services, configuration);
         AddServices(services);
         AddInfrastructure(services, configuration);
-        AddControllers(services);
         AddSwagger(services);
         AddIdentity(services);
         AddAuthentication(services, configuration);
-        AddJwtHandler(services);
-        AddOptions(services, configuration);
+
+        AddSyncfusion(configuration);
+
+        services.AddControllers(options => options.ReturnHttpNotAcceptable = true)
+            .AddXmlDataContractSerializerFormatters();
+        services.AddAutoMapper(typeof(CategoryMappings).Assembly);
+        services.AddHttpContextAccessor();
 
         return services;    
-    }
-
-    private static void AddControllers(IServiceCollection services)
-    {
-        services.AddControllers(options =>
-        {
-            options.ReturnHttpNotAcceptable = true;
-        }).AddXmlDataContractSerializerFormatters();
     }
 
     private static void AddSwagger(IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
+
         services.AddSwaggerGen(setup =>
         {
             var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var fullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
 
             setup.IncludeXmlComments(fullPath);
+
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme.",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+
+            setup.AddSecurityDefinition("Bearer", jwtSecurityScheme);
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                { jwtSecurityScheme, Array.Empty<string>() }
+            };
+
+            setup.AddSecurityRequirement(securityRequirement);
         });
     }
 
@@ -60,14 +82,13 @@ public static class DependencyInjection
         services.AddScoped<ISupplyService, SupplyService>();
         services.AddScoped<IDashboardService, DashboardService>();
 
-        services.AddAutoMapper(typeof(CategoryMappings).Assembly);
+        services.AddSingleton<JwtHandler>();
     }
 
     private static void AddInfrastructure(IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<WmsDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-        services.AddAutoMapper(typeof(CategoryMappings).Assembly);
     }
 
     private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
@@ -95,6 +116,8 @@ public static class DependencyInjection
                         Encoding.UTF8.GetBytes(jwtOptions["SecretKey"]))
                 };
             });
+
+        services.AddAuthorization();
     }
 
     private static void AddIdentity(IServiceCollection services)
@@ -110,16 +133,23 @@ public static class DependencyInjection
             .AddEntityFrameworkStores<WmsDbContext>();
     }
 
-    private static void AddJwtHandler(IServiceCollection services)
-    {
-        services.AddSingleton<JwtHandler>();
-    }
-
-    private static void AddOptions(IServiceCollection services, IConfiguration configuration)
+    private static void AddConfigurationOptions(IServiceCollection services, IConfiguration configuration)
     {
         services.AddOptions<JwtOptions>()
             .Bind(configuration.GetSection(JwtOptions.SectionName))
             .ValidateDataAnnotations()
             .ValidateOnStart();
+    }
+
+    private static void AddSyncfusion(IConfiguration configuration)
+    {
+        var key = configuration.GetValue<string>("Keys:Syncfusion");
+
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new InvalidOperationException("Cannot register Syncfusion without Key.");
+        }
+
+        SyncfusionLicenseProvider.RegisterLicense(key);
     }
 }
